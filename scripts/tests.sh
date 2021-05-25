75621d537531e113e724b2563977d793ec3cb2d1
Kam1runetz <notsoserious2017@gmail.com>
2021-05-25 16:20:31 +0300

    refactored

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -7,7 +7,7 @@ CMAKE_CONFIG_OPTS="-DCMAKE_BUILD_TYPE=Debug"
 CMAKE_TOOLCHAIN_OPTS="-DCMAKE_TOOLCHAIN_FILE='$(pwd)/tools/polly/gcc-pic-cxx17.cmake'"
 CMAKE_OPTS="$CMAKE_CONFIG_OPTS $CMAKE_TOOLCHAIN_OPTS"
 
-cmake -H. -B_builds -DBUILD_COVERAGE=ON "$CMAKE_OPTS"
+cmake -H. -B_builds -DBUILD_COVERAGE=ON -DBUILD_TESTS=ON "$CMAKE_OPTS"
 cmake --build _builds
 cmake --build _builds --target test -- ARGS="--verbose"
 gcovr -r . --xml coverage.xml
